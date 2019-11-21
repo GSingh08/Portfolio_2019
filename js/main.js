e857bd2028b898c8fc1cf3310be01658aff16822
Gurjot Singh <gurjot@myerrunds.com>
2019-11-20 19:19:20 -0500

    Updates

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//Select the DOM Items
+
+const menuButton = document.querySelector(".menu-btn");
+const menu = document.querySelector(".menu");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+
+//Set the initial state of the menu aka the overlay
+let showMenu = false;
+
+menuButton.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  if (!showMenu) {
+    menuButton.classList.add("close");
+    menu.classList.add("show");
+    menuBranding.classList.add("show");
+    menuNav.classList.add("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    showMenu = true;
+  } else {
+    menuButton.classList.remove("close");
+    menu.classList.remove("show");
+    menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
+    navItems.forEach(item => item.classList.add("show"));
+
+    showMenu = false;
+  }
+}
